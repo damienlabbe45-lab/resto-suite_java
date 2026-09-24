362466aa8a12209e7370cf0e702374aaa6020aae
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-24 11:20:00 +0200

    ajout d'un constructeur Food vide et de la méthode foodString

--- Food.java
+++ Food.java
@@ -7,9 +7,26 @@ public class Food{
         this.price = price;
     }
 
+    
+
+    public Food() {
+    }
+
+
+
     @Override
     public String toString() {
         return name + "   " + price + "euros \n\n";
+    }
+
+    public static String foodString(Food[] foods, String message){
+        int i =1;
+        for(Food food:foods){
+            message = message + i + ": " + food.name + " " + food.price + " euros ";
+        i++;
+        }
+        return message;
+        
     }
 
     
