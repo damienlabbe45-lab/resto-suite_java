6451df0d09327b2b49902d9bb9349437e11c8752
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-24 11:18:42 +0200

    changement dans commande de Resto.
    la variable commande est maintenant de type Food[],  les variables desserts, plats, boissons, accompagnements et  entrée ont été remplacé par une variable food de type Food[][].
    
    le long message en dur pour indiquer quel numéro taper pour choisir ont été remplacé par une méthode de la classe Food. enfin, la  variable intermédiaire com convertit en String le résultat de commande

--- Resto.java
+++ Resto.java
@@ -7,27 +7,90 @@ import java.util.stream.IntStream;
 
 public class Resto {
 	public static void commande(int[] persons, BufferedWriter file , Scanner input) throws IOException{
-		String [] commande = new String [5];
-		String [] entree = {"SALADE","SOUPE","QUICHE","MELON","TOAST DE MOUSSE DE CANARD", "RADIS","OLIVES","TOMATES","AUCUNE","SAUCISSON","SAUMON"};
-		String [] plats = {"POULET","VEAU","BOEUF","MOUTON","CANARD","AGNEAU","OEUFS","TOFU","AUCUN","JAMBON","GALETTES AVEC DES OEUFS, DES CHAMPIGNONS, DU JAMBON, DU BEURRE ET DU FROMAGE RÂPÉE"};
-		String [] accompagnements = {"FRITES","RIZ","PATES","CAROTTES","ORANGES BLEUES","POMME DE TERRE DAUPHINE","SALADE","HARICOTS VERTS","FLAGEOLETS","AUCUN","SALADE DE FRUITS COMPOSÉE DE MANGUES, BANANES, NOIX, RAISINS SECS, NECTARINES, FRAISES, FRAMBOISES, CERISES, KIWIS ET MANDARINES"};
-		String [] boissons = {"EAU","AUCUN","CIDRE","JUS D'ORANGE","WHISKY","VIN JAUNE","VIN ROUGE","VIN BLANC","FUZE TEA","JUS DE RAISIN","SIROP à L'EAU"};
-		String [] desserts = {"TARTE AUX POMMES","FONDANT AU CHOCOLAT","GATEAU AU YAOURT","CRÊPE AU SIROP D'ÉRABLE","MONT D'OR","ANANAS","MOUSSE AU CHOCOLAT","POMMES","COMPOTE DE POIRE","20 COOKIES DE 34 CM","AUCUN"};
+		Food [] commande = new Food [5];
+		Food[][] food = {{new Food("SALADE", 3.56), 
+		new Food("SOUPE", 2.01),
+		new Food("QUICHE", 7.89),
+		new Food("MELON", 4.56),
+		new Food("TOAST DE MOUSSE DE CANARD", 9.08), 
+		new Food("RADIS", 6.76), 
+		new Food("OLIVES", 5.43),
+		new Food("TOMATES", 3.78),
+		new Food("AUCUNE", 0.00),
+		new Food("SAUCISSON", 9.00),
+		new Food("SAUMON", 7.01)},
+	{new Food("POULET", 3.67),
+	new Food("VEAU", 11.98),
+	new Food("BOEUF", 20.65),
+	new Food("MOUTON", 14.89),
+	new Food("CANARD", 5.56),
+	new Food("AGNEAU", 3.99),
+	new Food("OEUFS", 4.43),
+	new Food("TOFU", 38.90),
+	new Food("AUCUN", 0.00),
+	new Food("JAMBON", 13.45),
+	new Food("GALETTES AVEC DES OEUFS, DES CHAMPIGNONS, DU JAMBON, DU BEURRE ET DU FROMAGE RÂPÉE", 5.01)
+	},
+	{
+		new Food("FRITES", 2.12),
+		new Food("RIZ",1.34),
+		new Food("PATES", 1.01),
+		new Food("CAROTTES", 2.08),
+		new Food("ORANGES BLEUES", 2.01),
+		new Food("POMME DE TERRE DAUPHINE", 1.98),
+		new Food("SALADE", 2.09),
+		new Food("HARICOTS VERTS", 1.55),
+		new Food("FLAGEOLETS",1.45),
+		new Food("AUCUN",0.00),
+		new Food("SALADE DE FRUITS COMPOSÉE DE MANGUES, BANANES, NOIX, RAISINS SECS, NECTARINES, FRAISES, FRAMBOISES, CERISES, KIWIS ET MANDARINES",5.09)
+	}, 
+	{
+		new Food("EAU", 0.00),
+		new Food("AUCUN",0.00),
+		new Food("CIDRE", 10.09),
+		new Food("JUS D'ORANGE", 5.09),
+		new Food("WHISKY", 45.89),
+		new Food("VIN JAUNE", 24.81),
+		new Food("VIN ROUGE", 25.80),
+		new Food("VIN BLANC", 25.59),
+		new Food("FUZE TEA", 6.78),
+		new Food("JUS DE RAISIN", 5.90),
+		new Food("SIROP à L'EAU", 3.89)
+	},
+	{
+		new Food("TARTE AUX POMMES", 8.34),
+		new Food("FONDANT AU CHOCOLAT", 7.98),
+		new Food("GATEAU AU YAOURT", 4.89),
+		new Food("CRÊPE AU SIROP D'ÉRABLE", 3.89),
+		new Food("MONT D'OR", 7.55),
+		new Food("ANANAS", 4.67),
+		new Food("MOUSSE AU CHOCOLAT", 3.67),
+		new Food("POMMES", 2.97),
+		new Food("COMPOTE DE POIRE", 3.12),
+		new Food("20 COOKIES DE 34 CM", 5.01),
+		new Food("AUCUN", 0.00)
+	}};
+	
 		for(int person:persons) {
 			System.out.println("repas numero " + person);
-			System.out.println("choix Entrée: \n [1 - SALADE] [2 - SOUPE] [3 - QUICHE] [4 - MELON] [5 - TOAST DE MOUSSE DE CANARD] [6 - RADIS] [7 - OLIVES] [8 - TOMATES] [9 - AUCUNE] [10 - SAUCISSON] [11 - SAUMON]\n vous prendrez quoi comme entrée? [SAISSISEZ LE NOMBRE CORRESPONDANT]");
-			commande[0] = entree[InputChoice(input)];
-			System.out.println("choix Plats: \n [1 - POULET] [2 - VEAU] [3 - BOEUF] [4 - MOUTON] [5 - CANARD] [6 - AGNEAU] [7 - OEUFS] [8 - TOFU] [9 - AUCUN] [10 - JAMBON] [11 - GALETTES AVEC DES OEUFS, DES CHAMPIGNONS, DU JAMBON, DU BEURRE ET DU FROMAGE RÂPÉE] \n vous prendrez quoi comme plats? [SAISSISEZ LE NOMBRE CORRESPONDANT]");
-			commande[1] = plats[InputChoice(input)];
-			System.out.println("choix accompagnements: \n [1 - FRITES] [2 - RIZ] [3 - PATES] [4 - CAROTTES] [5 - ORANGES BLEUES] [6 - POMMES DE TERRE DAUPHINE] [7 - SALADE] [8 - HARICOTS VERTS] [9 - FLAGEOLETS] [10 - AUCUN] [11 - SALADE DE FRUITS COMPOSÉE DE MANGUES, BANANES, NOIX, RAISINS SECS, NECTARINES, FRAISES, FRAMBOISES, CERISES, KIWIS ET MANDARINES] \n vous prendrez quoi pour accompagner vos plats? [SAISSISEZ LE NOMBRE CORRESPONDANT]");
-			commande[2] = accompagnements[InputChoice(input)];
-			System.out.println("choix boissons: \n [1 - EAU] [2 - AUCUN] [3 - CIDRE] [4 - JUS D'ORANGE] [5 - WHISKY] [6 - VIN JAUNE] [7 - VIN ROUGE] [8 - VIN BLANC] [9 - FUZE TEA] [10 - JUS DE RAISIN] [11 - SIROP À L'EAU] \n vous prendrez quoi comme boissons? [SAISSISEZ LE NOMBRE CORRESPONDANT]");
-			commande[3] = boissons[InputChoice(input)];
-			System.out.println("choix desserts: \n [1 - TARTE AUX POMMES] [2 - FONDANT AU CHOCOLAT] [3 - GATEAU AU YAOURT] [4 - CRÊPE AU SIROP D'ÉRABLE] [5 - MONT D'OR] [6 - ANANAS] [7 - MOUSSE AU CHOCOLAT] [8 - POMMES] [9 - COMPOTE DE POIRE] [10 - 20 COOKIES DE 34 CM] [11 - AUCUN] \n vous prendrez quoi comme desserts? [SAISSISEZ LE NOMBRE CORRESPONDANT]");
-			commande[4] = desserts[InputChoice(input)];
-			String com =commande[0] + "\n\n" + commande[1] + "\n\n" + commande[2] + "\n\n" + commande[3] + "\n\n" + commande[4];
-			System.out.println("Résumé du repas numéro " + person + ":\n\n" + com + "\n\n");
-			file.write("************  Résumé de la commande N°" + person + "********************\n\n" +com + "\n\n");
+			System.out.println(Food.foodString(food[0], "choix Entrée: \n"));
+			commande[0] = food[0][InputChoice(input)];
+
+			System.out.println(Food.foodString(food[1], "choix Plats: \n"));
+			commande[1] = food[1] [InputChoice(input)];
+
+			System.out.println(Food.foodString(food[2], "choix Accompagnements: \n"));
+			commande[2] = food[2] [InputChoice(input)];
+
+			System.out.println(Food.foodString(food[3], "choix Boissons: \n"));
+			commande[3] = food[3] [InputChoice(input)];
+
+			System.out.println(Food.foodString(food[4], "choix Desserts: \n"));
+			commande[4] = food[4] [InputChoice(input)];
+
+			String com =commande[0].toString() + commande[1].toString() + commande[2].toString() + commande[3].toString() + commande[4].toString();
+			System.out.println("Résumé du repas numéro " + person + ":\n\n" + com);
+			file.write("************  Résumé de la commande N°" + person + "********************\n\n" +com);
 	
 		}
 	}
