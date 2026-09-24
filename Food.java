f48b82752edf4b475832b2421177b6336934b3ed
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-24 11:30:17 +0200

    ajoutde la méthode priceCommande. légère modification pour rajouter  un espace dans toString et foodString

--- Food.java
+++ Food.java
@@ -16,17 +16,23 @@ public class Food{
 
     @Override
     public String toString() {
-        return name + "   " + price + "euros \n\n";
+        return name + "   " + price + " euros \n\n";
     }
 
     public static String foodString(Food[] foods, String message){
         int i =1;
         for(Food food:foods){
-            message = message + i + ": " + food.name + " " + food.price + " euros ";
+            message = message + i + ": " + food.name + " " + food.price + " euros \n";
         i++;
         }
         return message;
         
+    }
+
+    public static Double priceCommande(Food[] foods){
+        Double pricetotal = 0.00;
+        for(Food food:foods) pricetotal = pricetotal + food.price;
+        return pricetotal;
     }
 
     
